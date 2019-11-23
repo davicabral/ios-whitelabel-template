db35af458e5bab7740e6b88fffaaa1b063566a49
Davi Cabral <davi_cabral@atlantico.com.br>
2019-11-23 12:24:17 -0300

    Project creation

---
+++ WhiteLabel-Template/WhiteLabel-Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteLabel-Template
+//
+//  Created by Davi Cabral de Oliveira on 23/11/19.
+//  Copyright © 2019 Davi Cabral de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
